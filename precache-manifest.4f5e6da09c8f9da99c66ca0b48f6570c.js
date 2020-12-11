be72c4ee5d9f769be3560d387ecbeb89dab5a9cf
mrtung1999hpvn <tung75397@st.vimaru.edu.vn>
2020-12-11 21:42:54 +0700

    Updates

---
+++ precache-manifest.4f5e6da09c8f9da99c66ca0b48f6570c.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0741f8e23d5cb5e6ee19d1cd652f619d",
+    "url": "/NhanDangCuChiTay/index.html"
+  },
+  {
+    "revision": "86d245f41d0e4adcb429",
+    "url": "/NhanDangCuChiTay/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "a4ccff6640a07c90fb53",
+    "url": "/NhanDangCuChiTay/static/js/2.e6bd9e86.chunk.js"
+  },
+  {
+    "revision": "93ce851949c81c033c28f6758a3c33f3",
+    "url": "/NhanDangCuChiTay/static/js/2.e6bd9e86.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "86d245f41d0e4adcb429",
+    "url": "/NhanDangCuChiTay/static/js/main.26f3a16d.chunk.js"
+  },
+  {
+    "revision": "0fd7933a321c94ed5296",
+    "url": "/NhanDangCuChiTay/static/js/runtime-main.0ee57cbe.js"
+  },
+  {
+    "revision": "1cfa24a5e82c665b700f38d9bdf04195",
+    "url": "/NhanDangCuChiTay/static/media/thumbs_up.1cfa24a5.png"
+  },
+  {
+    "revision": "6db44b04476f1385864179e398b16fa0",
+    "url": "/NhanDangCuChiTay/static/media/victory.6db44b04.png"
+  }
+]);
